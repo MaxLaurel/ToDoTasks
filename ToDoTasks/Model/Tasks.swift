a1a3c28929d05f5b08eecfd406c650b3e72288ab
trainee <max.filippov.3333@gmail.com>
2023-12-29 00:02:16 +0300

    kkkkkkk

---
+++ ToDoTasks/Model/Tasks.swift
@@ -0,0 +1,9 @@
+//
+//  Tasks.swift
+//  ToDoTasks
+//
+//  Created by Максим on 24.12.2023.
+//
+
+import Foundation
+import Firebase
---
+++ ToDoTasks/Model/Users.swift
@@ -0,0 +1,9 @@
+//
+//  Users.swift
+//  ToDoTasks
+//
+//  Created by Максим on 24.12.2023.
+//
+
+import Foundation
+import Firebase
