78f7497872708d129446d9616513a2f11ae1e527
MaxLaurel <max.laurel.developer@gmail.com>
2024-01-13 18:41:00 +0300

    13/01/2024

--- ToDoTasks/Controllers/LoginViewController.swift
+++ ToDoTasks/Controllers/LoginViewController.swift
@@ -10,7 +10,7 @@ import Firebase
 
 
 class LoginViewController: UIViewController, UITextFieldDelegate {
- 
+ //////////////////////
     private var mainLabel: UILabel = {
         var mainLabel = UILabel()
         mainLabel.text = "ToDoTask"
